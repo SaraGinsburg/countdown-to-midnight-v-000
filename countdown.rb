278577a037db6b92ed52a4df69c60cdaf6c502e7
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-06 21:54:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,15 @@
 #write your code here
 
 def countdown(sec)
+  while sec > 0
+    put "#{sec} SECOND(S)!"
+    sec -= 1
+    countdown_with_sleep
+  end
+  "HAPPY NEW YEAR!"  
+
 end
 
 def countdown_with_sleep
+  sleep(1)
 end
